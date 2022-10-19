c870e510cc0fecbe489f4d64f415e4ab08ba815f
josephfeussi <feussilassi@gmail.com>
2022-10-19 02:33:53 +0200

    Added register btn for xm screens

--- js/my_js.js
+++ js/my_js.js
@@ -8,3 +8,21 @@ window.intlTelInput(input,({
   separateDialCode:false,
   preferredCountries: ["cm","us","gb" ],
 }));
+
+
+formElem.onsubmit = async (e) => {
+  e.preventDefault();
+  
+  let response = await fetch('https://joan500.pythonanywhere.com/api/landing/landing', {
+    method: 'POST',
+    body: new FormData(formElem)
+  });
+  console.log("Send submitted");
+  document.getElementById("formElem").reset();
+
+  let result = await response.json();
+  //console.log(result)
+  
+  alert(result.message);
+  alert('TEst');
+};
