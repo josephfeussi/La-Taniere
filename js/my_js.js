1c855776d19d37899e75486b335ff564461d2cd9
josephfeussi <feussilassi@gmail.com>
2022-10-21 21:01:43 +0200

    Registration form fonctional

--- js/my_js.js
+++ js/my_js.js
@@ -1,28 +1,46 @@
 // Vanilla Javascript
 var input = document.querySelector("#phone");
-window.intlTelInput(input,({
+window.intlTelInput(input, {
   // options here
-  allowDropdown:true,
-  autXoPlaceholder:"polite",
-  placeholderNumberType:"MOBILE",
-  separateDialCode:false,
-  preferredCountries: ["cm","us","gb" ],
-}));
+  allowDropdown: true,
+  autXoPlaceholder: "polite",
+  placeholderNumberType: "MOBILE",
+  separateDialCode: false,
+  preferredCountries: ["cm", "us", "gb"],
+});
 
+const form = document.getElementById("myForm");
+document.getElementById("myAlert").style = "display:none";
 
-formElem.onsubmit = async (e) => {
-  e.preventDefault();
+function submit_record() {
+  var fd = new FormData(form);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://joan500.pythonanywhere.com/api/landing/landing");
+  if (fd.get("first_name") != "" && fd.get("last_name")!="" && fd.get("email")!="" && fd.get("phone")!=""){
+    xhr.send(fd);
+    xhr.onreadystatechange = () => {
+      // In local files, status is 0 upon success in Mozilla Firefox
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        const status = xhr.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          // The request has been completed successfully
+          document.getElementById("myAlert").style = "display:none";
+          console.log(xhr.responseText);
+          console.log("The request has been completed successfully");
+          user_name = fd.get("first_name");
+          form.reset();
+          alert(user_name + " , vous avez été enregistré.");
+        } else {
+          // Oh no! There has been an error with the request!
+          console.log("Error");
+          alert(user_name + "Erreur , veuillez essayer à nouveau.");
+        }
+      }
+    };
+  } else{
+    alert("Veuillez remplir vos informations.");
+  }
   
-  let response = await fetch('https://joan500.pythonanywhere.com/api/landing/landing', {
-    method: 'POST',
-    body: new FormData(formElem)
-  });
-  console.log("Send submitted");
-  document.getElementById("formElem").reset();
-
-  let result = await response.json();
-  //console.log(result)
-  
-  alert(result.message);
-  alert('TEst');
-};
+ 
+}
